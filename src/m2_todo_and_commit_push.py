bd89dbcd9ad92721d97c0f223f08b15fdb1ba196
ahmedrh <ahmedrh@rose-hulman.edu>
2023-09-04 20:05:26 -0400

    Done

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -74,7 +74,7 @@ print('first line')
 ###############################################################################
 print(3607*34227)
 ###############################################################################
-# TODO: 5.
+# done: 5.
 #   Look at the list of files in the Project window (to the left).
 #   Note that this file:
 #      m2_todo_and_commit_push.py
