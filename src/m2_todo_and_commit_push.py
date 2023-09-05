09870f790f86a4514c265e9d8bae581693602f6c
ahmedrh <ahmedrh@rose-hulman.edu>
2023-09-04 20:03:29 -0400

    Done

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -15,7 +15,7 @@
 print("Hello, World")
 
 ###############################################################################
-# TODO: 1.
+# Done: 1.
 #   (Yes, that means for YOU to DO things per these instructions:)
 #  _
 #   Run this module by right clicking anywhere in this window
@@ -29,7 +29,7 @@ print("Hello, World")
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# done: 2.
 #   Notice the small horizontal BLUE bars on the scrollbar-like thing
 #   on the right.  Each blue bar indicates a thing "to do" in this module.
 #  _
@@ -52,16 +52,16 @@ print("Hello, World")
 ###############################################################################
 
 ###############################################################################
-# TODO: 3.
+# done: 3.
 #   Add another   print   statement below this comment.
 #   It should print any string that you want (but keep it polite and G-rated!)
 #  _
 #   Test your code by re-running this module.
 #   Look at the  Run  window to be sure that your string printed as expected.
 ###############################################################################
-
+print('first line')
 ###############################################################################
-# TODO: 4.
+# done: 4.
 #   Add yet another   print   statement, putting it below this comment.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
@@ -72,7 +72,7 @@ print("Hello, World")
 #      What number did your   print   display for this _TODO_?
 #   (HINT: It is an INTERESTING number.)  Get help if your value is wrong.
 ###############################################################################
-
+print(3607*34227)
 ###############################################################################
 # TODO: 5.
 #   Look at the list of files in the Project window (to the left).
